ce0c0dea585a4ce99ca7a34f46d1557101b5b58c
mike stedman <github@ravuya.com>
2016-05-19 21:38:03 -0600

    initial version, chops 30 seconds out at 12fps

---
+++ gif-slicer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FFMPEG=ffmpeg
+FFPROBE=ffprobe
+CLIP_LENGTH=30
+FPS=11
+
+if [ -n "$1" ]
+then
+	echo "Video provided:" $1;
+	# find out how long the video is
+	duration_secs=$($FFPROBE -i "$1" -show_format 2>&1 | grep duration | awk -F "=" '{print $2}')
+	# round to full seconds
+	duration_secs=$( printf "%.0f" $duration_secs ) 
+	# generate a random number between [0, duration_secs - 30)
+	max_time=$(($duration_secs - $CLIP_LENGTH))
+	start_time=$(( RANDOM % $max_time ))
+	# ok now let that shit run
+	$FFMPEG -i "$1" -ss $start_time -t $CLIP_LENGTH -vf fps=$FPS img%03d.jpg
+fi
